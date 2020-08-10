ea58e4b2493056ea01505eb2c374c3c401caaafe
Turetska <oturetska@yahoo.com>
2020-08-10 16:11:00 -0400

    Updates

---
+++ precache-manifest.e8c5fd29ba922f63eb41aaac36ecdb1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f9bf74f8413d96902b9b3e9916df5e",
+    "url": "/Employee-Directory_React/index.html"
+  },
+  {
+    "revision": "b289b2d56cebef3a3ee8",
+    "url": "/Employee-Directory_React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2598a2f87656e78a5ef4",
+    "url": "/Employee-Directory_React/static/js/2.4ad2417f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory_React/static/js/2.4ad2417f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b289b2d56cebef3a3ee8",
+    "url": "/Employee-Directory_React/static/js/main.31e564de.chunk.js"
+  },
+  {
+    "revision": "c2b9fa446d9ac4d58f93",
+    "url": "/Employee-Directory_React/static/js/runtime-main.bbc6bdf9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Employee-Directory_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
